ba4f1fc7f48649ede033856e6d3a4a6e211cf255
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2022-03-24 15:15:45 -0400

    ENH: Create helper method for easier access to DB using app context

--- niviz_rater/db.py
+++ niviz_rater/db.py
@@ -1,22 +1,28 @@
 from peewee import SqliteDatabase
-from typing import Any, List, Optional
-
-# As much as it would be nice to have this be passed into the app via a
-# configuration file, but peewee Models are not compatible with this
-# To use an in-memory db, use 'file::memory:?cache=shared'
-DB_NAME: str = 'test.db'
-
-sqlite_db: Optional[SqliteDatabase] = None
+from typing import Any, List, Optional, Callable
 
 
-def get_or_create_db(additional_pragmas: Optional[List[Any]] = None
-                     ) -> SqliteDatabase:
-    global sqlite_db
-    if sqlite_db:
-        return sqlite_db
+def get_or_create_db(
+        db_str: str,
+        additional_pragmas: Optional[List[Any]] = None) -> SqliteDatabase:
+
     pragmas = [('foreign_keys', 'on')]
     if additional_pragmas:
         pragmas.append(additional_pragmas)
-    sqlite_db = SqliteDatabase(DB_NAME, pragmas=pragmas, uri=True)
+
+    sqlite_db = SqliteDatabase(db_str, pragmas=pragmas, uri=True)
     return sqlite_db
 
+
+def fetch_db_from_config(app_config,
+                         additional_pragmas: Optional[List[Any]] = None):
+    """
+    Fetch database by first trying to pull from
+    stored application configuration and if fail, then
+    resort to requesting one using db_file
+    """
+
+    return app_config.get(
+        'niviz_rater.db.instance',
+        get_or_create_db(app_config['niviz_rater.db.file'],
+                         additional_pragmas))

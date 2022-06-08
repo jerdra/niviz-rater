5091f399d06110e10165c0f8157906c1662bbd5e
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2022-06-08 17:45:20 -0400

    TST/FIX: make sure all tables are dropped before closing connection to avoid test leakage

--- niviz_rater/db/tests/conftest.py
+++ niviz_rater/db/tests/conftest.py
@@ -1,9 +1,15 @@
 import pytest
 from peewee import SqliteDatabase
 from niviz_rater.db.models import database_proxy
+import niviz_rater.db.models as models
 
 DB = SqliteDatabase(":memory:")
 database_proxy.initialize(DB)
+
+MODELS = [
+    models.Rating, models.Component, models.Entity, models.Image,
+    models.TableRow, models.TableColumn, models.Annotation
+]
 
 
 @pytest.fixture
@@ -13,4 +19,6 @@ def db():
     """
     DB.connect()
     yield DB
+
+    DB.drop_tables(MODELS)
     DB.close()

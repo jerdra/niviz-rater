7bf9676f2eb52e0a0e5038b382951403bcc4da9b
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2022-06-14 09:33:06 -0400

    TST: Set up first test, failing

--- niviz_rater/db/tests/test_utils.py
+++ niviz_rater/db/tests/test_utils.py
@@ -1,12 +1,10 @@
 from unittest.mock import patch
 from peewee import SqliteDatabase
+from string import Template
 from niviz_rater.db.models import database_proxy
 import niviz_rater.db.models as models
 import niviz_rater.db.utils as dbutils
-
-# Use in-memory DB for testing purposes
-DB = SqliteDatabase(":memory:")
-database_proxy.initialize(DB)
+import niviz_rater.spec as spec
 
 
 def test_ratings_are_initialized_with_settings(db):
@@ -22,8 +20,34 @@ def test_ratings_are_initialized_with_settings(db):
     assert set(expected_ratings) == set(found_ratings)
 
 
-def test_create_or_update_entity_skips_update_if_flag_not_set(db):
-    assert False
+def test_create_or_update_entity_skips_update_if_flag_not_set(configured_db):
+
+    db, settings = configured_db
+    new_name = "NEW_NAME"
+
+    tablerow = models.TableRow.get_by_id(1)
+    tablecol = models.TableColumn.get_by_id(1)
+    component = models.Component.get_by_id(1)
+    qc_entity = spec.QCEntity(images=[],
+                              entities={},
+                              tpl_label="NEW_NAME",
+                              tpl_column_name=Template(
+                                  settings['column_name']),
+                              tpl_row_name=Template(settings['row_name']))
+
+    dbutils.create_or_update_entity(db,
+                                    component=component,
+                                    qc_entity=qc_entity)
+
+    entity = models.Entity.get_by_id(1)
+    expected_rating = models.Rating.get(
+        models.Rating.name == settings['rating_name'])
+    expected_annotation = models.Annotation.get(
+        models.Annotation.name == settings['annotation_name'])
+
+    assert entity.name == settings['entity_name']
+    assert entity.rating == expected_rating
+    assert entity.annotation == expected_annotation
 
 
 def test_create_or_update_entity_update_if_update_existing(db):

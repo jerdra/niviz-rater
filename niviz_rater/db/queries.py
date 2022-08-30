0317805312f41ef7af94986a6f19575b20e2c871
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2022-08-30 19:19:01 -0400

    STY/ENH: Apply yapf and get_summary() function

--- niviz_rater/db/queries.py
+++ niviz_rater/db/queries.py
@@ -1,42 +1,59 @@
 from __future__ import annotations
 
-from typing import Optional
+from typing import Optional, Tuple
 import logging
-import niviz_rater.db.models as models
+from peewee import JOIN
+from niviz_rater.db.models import (
+        Entity, Component, TableColumn, TableRow
+        )
 
 
 logger = logging.getLogger(__name__)
 
 
-def get_component(component_name: str, create=False) -> models.Component:
+def get_component(component_name: str, create=False) -> Component:
 
     if create:
-        component = models.Component.get_or_create(
-            models.Component.name == component_name)
+        component = Component.get_or_create(
+            Component.name == component_name)
     else:
-        component = models.Component.get(
-            models.Component.name == component_name)
+        component = Component.get(
+            Component.name == component_name)
 
     return component
 
 
 def get_entity_by_row_col(row_name: str,
-                          col_name: str) -> Optional[models.Entity]:
+                          col_name: str) -> Optional[Entity]:
     """
     Return an Entity by it's unique row/col combination
     """
 
-    result = models.Entity.select(models.Entity, models.TableColumn,
-                                  models.TableRow) \
-        .join(models.TableRow) \
-        .switch(models.Entity) \
-        .join(models.TableColumn) \
-        .switch(models.Entity) \
-        .where((models.TableColumn.name == col_name) &
-               (models.TableRow.name == row_name))
+    result = Entity.select(Entity, TableColumn,
+                                  TableRow) \
+        .join(TableRow) \
+        .switch(Entity) \
+        .join(TableColumn) \
+        .switch(Entity) \
+        .where((TableColumn.name == col_name) &
+               (TableRow.name == row_name))
 
     if len(result) > 1:
         logger.error(f"Expected 1 result but got {len(result)}\n"
                      "Only returning first result")
 
     return result.first()
+
+def get_summary() -> Tuple[int, int, int]:
+    """
+    Get number of Entities that have yet to be rated
+
+    Returns:
+        summary: (total_entities, number_rated, number_unrated)
+    """
+
+    total = Entity.select().count()
+    n_unrated = Entity.select().where(Entity.rating.is_null()).count()
+    n_rated = total - n_unrated
+
+    return total, n_rated, n_unrated

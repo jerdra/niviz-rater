4deedcdf0e28548e3fe77332c74f8fc090cabe0f
Jerry Jeyachandra <jerrold.jeyachandra@gmail.com>
2022-08-30 20:00:25 -0400

    TST: Make entity creation more ergonomic in tests

--- niviz_rater/db/tests/test_queries.py
+++ niviz_rater/db/tests/test_queries.py
@@ -2,6 +2,16 @@ import niviz_rater.db.queries as queries
 import niviz_rater.db.models as models
 
 
+def _create_entity_column(name, columnname, foreign_keys, rating=None):
+
+    tc = models.TableColumn(name=columnname)
+    tc.save()
+    return models.Entity.create(name=name,
+                                columnname=tc,
+                                rating=rating,
+                                **foreign_keys)
+
+
 def test_summary_returns_correct_number_of_entities(configured_db):
     """
     Correct number of rated, unrated, and total number of entities
@@ -9,27 +19,18 @@ def test_summary_returns_correct_number_of_entities(configured_db):
     """
 
     db, settings, foreign_keys = configured_db
-
-    tc1 = models.TableColumn(name="acolumnname")
-    tc2 = models.TableColumn(name="anothercolumnname")
-    with db.atomic():
-        tc1.save()
-        tc2.save()
-
-    models.Entity.create(name="anentity",
-                         columnname=tc1,
-                         rowname=foreign_keys["rowname"],
-                         component=foreign_keys["component"])
-
-    a_rating = models.Rating.get_by_id(1)
-    models.Entity.create(name="anentity",
-                         columnname=tc2,
-                         rowname=foreign_keys["rowname"],
-                         component=foreign_keys["component"],
-                         rating=a_rating)
+    _create_entity_column("anentity", "acolumnname", foreign_keys)
+    rating = models.Rating.get_by_id(1)
+    _create_entity_column("anotherentity", "anothercolumnname", foreign_keys,
+                          rating)
 
     total, n_rated, n_unrated = queries.get_summary()
 
     assert total == 3
     assert n_rated == 2
     assert n_unrated == 1
+
+
+def test_denormalized_entities_returns_fully_populated_models(configured_db):
+
+    db, settings, foreign_keys = configured_db
